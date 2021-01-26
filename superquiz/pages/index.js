224dd8733fd7714934e63fd71010863055c6db18
Geraldo Marink <geraldo@camponovo.ro.gov.br>
2021-01-26 16:08:01 -0400

    ajuste aula 1

--- superquiz/pages/index.js
+++ superquiz/pages/index.js
@@ -16,7 +16,7 @@ import TagMeta from '../src/components/Meta';
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
-  padding-top: 200px;
+  padding-top: 100px;
   margin: auto 10%;
   @media screen and (max-width: 500px) {
     margin: auto;
